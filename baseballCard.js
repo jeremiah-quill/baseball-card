201afa96d3fe7233264d4d8ea98722f7f53e49d7
jeremiah-quill <53875206+jeremiah-quill@users.noreply.github.com>
2019-08-12 09:57:26 -0400

    Refactored JS
    Combined 4 functions into 1

--- baseballCard.js
+++ baseballCard.js
@@ -1,40 +1,37 @@
-function printName(message) {
-    var outputDiv = document.getElementById("name);
-    outputDiv.innerHTML = message;
-}
-function printPosition(message) {
-    var outputDiv = document.getElementById("position");
-    outputDiv.innerHTML = message;
-}
-function printTeam(message) {
-    var outputDiv = document.getElementById("team");
-    outputDiv.innerHTML = message;
-}
+var name = prompt("Please choose a player name:");
+var position = prompt("Please choose a player position:");
+var number = prompt("Please choose a number:");
+var team = prompt("Please choose a team name:");
+var color = prompt("Please choose between yellow, red, blue, green, black, orange, or purple:");
+var pic = prompt("Please choose dog, cat, or monkey:");
+var idKey = ['name', 'position', 'number', 'team'];
+var inputKey = [name, position, number, team];
 
+
+function printPic() {
+if (pic === "dog") {
+    document.getElementById("card-container").style.background = "url('dog2.jpg') no-repeat center";
+} else if (pic ==="cat"){
+    document.getElementById("card-container").style.background = "url('cat.jpg') no-repeat center";
+} else {
+    document.getElementById("card-container").style.background = "url('monkey.jpg') no-repeat center";
+    }
+}
 function printColor() {
     if(color === "black" || color === "purple" || color === "blue") {
         document.getElementById("border-container").style.color = "white";
     };
     document.getElementById("border-container").style.backgroundColor = color;
 }
-
-function printPic() {
-if (pic === "dog") {
-    document.getElementById("card-container").style.background = "url('dog2.jpg') no-repeat center";
-} else {
-    document.getElementById("card-container").style.background = "url('cat.jpg') no-repeat center";
-}
+function addText() {
+    for (i=0; i < idKey.length; i += 1) {
+        var outputDiv=document.getElementById(idKey[i]);
+        outputDiv.innerHTML = inputKey[i];
+        }
 }
 
-var name = prompt("Please choose a player name:");
-var position = prompt("Please choose a player position:");
-var team = prompt("Please choose a team name:");
-var color = prompt("Please choose between yellow, red, blue, green, black, orange, or purple:");
-var pic = prompt("Please choose dog or cat:");
 
 
-printName(name);
-printPosition(position);
-printTeam(team);
+printPic();
 printColor();
-printPic()
+addText();
